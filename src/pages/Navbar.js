0a434ba1e0b5e85671d4a354357c3660d92743ce
chokc03 <chokc03@gmail.com>
2022-02-14 21:07:38 +0900

    navbar complete

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,28 +1,31 @@
 import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
-import {IoIosArrowForward} from 'react-icons/io'
+import {IoIosArrowBack} from 'react-icons/io'
 import '../styles/Navbar.scss';
 
 function Navbar() {
-    const[toggleNav,setToggleNav] = useState("false");
+    const[toggleNav,setToggleNav] = useState(false);
     const handleNavbar=()=>{
-        setToggleNav(!toggleNav)
+        setToggleNav(value=>!value)
+    }
+    const removeActive=()=>{
+        setToggleNav(false);
     }
     
     return (
         <div className={toggleNav?'navbarContainer':'navbarContainer Close'}>
             <div className='Navbar'>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><a href="https://github.com/chokc03" target="_blank" rel="noreferrer">Github</a></li>
+                    <li onClick={removeActive}><Link to="/">Home</Link></li>
+                    <li onClick={removeActive}><Link to="/projects">Projects</Link></li>
+                    <li onClick={removeActive}><Link to="/about">About</Link></li>
+                    <li onClick={removeActive}><a href="https://github.com/chokc03" target="_blank" rel="noreferrer">Github</a></li>
                 </ul>
             </div>
             <div className='toggleBtn'>
                 <div className='circleBtn' onClick={handleNavbar}>
                     <div className={toggleNav?'arrow':' arrow close'}>
-                        <IoIosArrowForward/>
+                        <IoIosArrowBack/>
                     </div>
                 </div>
             </div>
